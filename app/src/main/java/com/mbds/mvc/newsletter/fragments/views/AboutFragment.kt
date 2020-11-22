06bce91dc8f48ee89c26f336842192350cc9bb42
TRAORE <mohamedtra96@gmail.com>
2020-11-22 07:55:17 +0100

    add view and fragment about

---
+++ app/src/main/java/com/mbds/mvc/newsletter/fragments/views/AboutFragment.kt
@@ -0,0 +1,7 @@
+package com.mbds.mvc.newsletter.fragments.views
+
+import androidx.fragment.app.Fragment
+import com.mbds.mvc.newsletter.R
+
+class AboutFragment : Fragment(R.layout.fragment_view_about) {
+}
