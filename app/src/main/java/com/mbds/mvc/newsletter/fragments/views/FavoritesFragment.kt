4ca346a7f32cd87770fc17f0e68cde6ac6194cd1
TRAORE <mohamedtra96@gmail.com>
2020-11-22 08:03:01 +0100

    add view and favorites fragment

---
+++ app/src/main/java/com/mbds/mvc/newsletter/fragments/views/FavoritesFragment.kt
@@ -0,0 +1,19 @@
+package com.mbds.mvc.newsletter.fragments.views
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.mbds.mvc.newsletter.R
+
+class FavoritesFragment: Fragment(R.layout.fragment_view_favorites) {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
